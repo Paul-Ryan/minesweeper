2f340286f9b1fbf49c96c4f6355da259656889ea
Paul <paul.ryan.iii@gmail.com>
2017-12-12 10:02:34 -0800

    work on board

--- board.rb
+++ board.rb
@@ -1,9 +1,24 @@
+require_relative 'tile'
+
 class Board
 
   attr_accessor :grid
 
   def initialize
-    @grid = Array.new(8) { Array.new(8) {nil} }
+    @grid = Array.new(9) { Array.new(9) {nil} }
 
+  end
+
+  def [](pos)
+    @grid[pos.first][pos.last]
+  end
+
+  def []=(pos, val)
+    @grid[pos.first][pos.last] = val
+  end
+
+  def place_bombs(grid)
+    pos = [(0..8).to_a.sample, (0..8).to_a.sample]
+    9.times
   end
 end
---
+++ tile.rb
@@ -0,0 +1,8 @@
+class Tile
+
+  def initialize(bomb = false)
+    @flagged = false
+    @bomb = bomb
+  end
+
+end
